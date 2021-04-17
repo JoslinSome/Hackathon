2938b76ef3687e87ae06ecfaeeeaf3bed382d0d3
Joslin Some <69180570+JoslinSome@users.noreply.github.com>
2021-04-16 23:09:02 -0500

    Add files via upload

---
+++ KvString.py
@@ -0,0 +1,268 @@
+from kivy.lang.builder import Builder
+
+CircleProgress = Builder.load_string("""
+<Bar>:
+""")
+screenHelp = """
+#: import FadeTransition kivy.uix.screenmanager.FadeTransition
+#: import SlideTransition kivy.uix.screenmanager.SlideTransition
+Screen:
+
+
+    
+    MDBottomNavigation:
+        id: panel
+        panel_color: 1, 1, 1, 1
+        MDBottomNavigationItem:
+            name: 'recur'
+            id: recur
+            on_tab_release: app.tab_switchRecur()
+            text: 'Recurring'
+            icon: 'calendar-month'
+            
+            MDFloatingActionButton:
+                id: recurBtn
+                icon: "plus"
+                pos_hint: {"center_x":0.12,"center_y":0.1}
+                md_bg_color: app.theme_cls.primary_color
+                elevation_normal: 10
+                user_font_size: "20sp"
+                on_release: app.addRecurringItem()
+        MDBottomNavigationItem:
+            name: 'track'
+            text: 'Track'
+            on_tab_release: app.tab_switchTrack()
+            id:track
+            icon: 'book-open-page-variant'
+            
+
+        MDBottomNavigationItem:
+            id:save
+            name: 'save'
+            text: 'Save'
+            icon: 'trophy'
+           
+        MDBottomNavigationItem:
+            id:overview
+            name: 'overview'
+            text: 'Overview'
+            on_tab_release: app.tab_switchView()
+            icon: 'view-grid-outline'
+            
+    BoxLayout:
+        orientation: "vertical"
+        MDToolbar:
+            title: "Demo App"
+
+            id: toolbar
+            left_action_items: [["menu",lambda x: nav_drawer.toggle_nav_drawer()]]
+            right_action_items: [["help-circle-outline",lambda x: app.helpReccuring()]]
+            size_hint_y: None
+            elevation: 10
+
+        NavigationLayout:
+            id: layout
+            ScreenManager:
+                id:manage
+                viewAll:
+            MDNavigationDrawer:
+                id: nav_drawer
+                size_hint_x: None   
+
+                width: 300
+
+                BoxLayout:
+                    orientation: "vertical"
+                    ScrollView:
+                        MDList:
+                            OneLineIconListItem:
+                                text: "Theme"
+                                on_release: app.ThemeChange()
+                                IconLeftWidget:  
+                                    icon:"format-color-text"
+                    Widget:
+                    Widget:
+    ScreenManager:
+        MenuScreen:
+
+            Widget:
+            NavigationLayout:
+                id: layout
+                ScreenManager:
+                    id:manage
+                    viewAll:
+                MDNavigationDrawer:
+                    id: nav_drawer2
+                    size_hint_x: None    
+                    width: 300
+                    BoxLayout:
+                        orientation: "vertical"
+                        Image: 
+
+                        Widget:
+                        Widget:
+
+        ProfileScreen:
+            id: profile
+        UploadScreen:
+
+<MenuScreen>:
+    name: 'menu'
+
+<UploadScreen>:
+    name: 'upload'
+
+<ProfileScreen>:
+
+
+"""
+dialogBox = """
+Content:
+    id:Content
+    name: name
+    cost: cost
+    orientation: "vertical"
+    spacing: "1dp"
+    size_hint_y: None
+    height: "200dp"
+    MDTextField:
+        id: name 
+        hint_text: "Item Name"
+        helper_text: "Example: Netflix,Rent"
+        helper_text_mode: "on_focus"
+        pos_hint:{"center_x":0.5,"center_y":0.8}
+    MDTextField:
+        hint_text: "Item Cost"
+        text: "$"
+        id: cost
+        helper_text: "Please enter a value"
+        helper_text_mode: "on_error"
+        halign: "auto"
+        pos_hint:{"center_x":0.5,"center_y":0.8}
+    MDFlatButton:
+        text: "Date:"
+        on_press: app.show_date_picker()
+        text_color: app.theme_cls.primary_color
+"""
+occurrence = """
+MDLabel:
+    text: app.recurringday
+"""
+
+dialogBox1 = """
+<Content1>:
+    id: Content1
+    orientation: "vertical"
+    size_hint_y: None
+    height: "200dp"
+    BoxLayout:
+        orientation: "vertical"
+        MDLabel:
+            text: "Item Name: "+  app.tempName
+            theme_text_color: "Custom"
+            text_color: app.theme_cls.primary_color
+        MDLabel:
+            text: "Item Cost: "+  app.tempCost
+            theme_text_color: "Custom"
+            text_color: app.theme_cls.primary_color
+        MDLabel:
+            text: "Occurs Monthly on the "+ app.recurringday
+            theme_text_color: "Custom"
+            text_color: app.theme_cls.primary_color
+
+<help>:
+    MDLabel:
+        text: "Track your recurring monthly payments like Rent and Netflix. Click on the plus icon at the bottom left of the screen to add a recurring item!"
+        theme_text_color: "Secondary"
+        pos_hint: {"center_y": 0.2}
+<helpTrack>:
+    MDLabel:
+        text: "Keep track of the money you gain and spend, press the buttons below to create an Item"
+        theme_text_color: "Secondary"
+        pos_hint: {"center_y": 0.2}
+<helpOverview>:
+    MDLabel:
+        text: "View your monthly spendings so far. First create a spending item on the reccuring or track tab before being able to view"
+        theme_text_color: "Secondary"
+        pos_hint: {"center_y": 0.2}
+<helpSave>:
+    MDLabel:
+        text: "Want to save up for something?, create a new item and keep track of how much hou have saved"
+        theme_text_color: "Secondary"
+        pos_hint: {"center_y": 0.2}
+<Existing>:
+    MDLabel:
+        text: "The Item you entered already exists. Please change its Name" 
+<Content2>:
+    title: "hello" 
+    name: name
+    cost: cost
+    BoxLayout:
+        spacing: "20dp"
+        MDTextField: 
+            id: name
+            hint_text: "Item Name:"
+        MDTextField:
+            hint_text: "Item Cost:"
+            id:cost
+            text: "$"
+
+<Content3>:
+    month:month
+    MDTextField:
+        id: month
+        hint_text: "Amount"
+        text: "$"
+
+<Content4>:
+    title: "hello" 
+    name: name
+    cost: cost
+    BoxLayout:
+        spacing: "20dp"
+        MDTextField: 
+            id: name
+            hint_text: "Item Name:"
+        MDTextField:
+            hint_text: "Item Cost:"
+            id:cost
+            text: "$"
+<Content5>:
+    title: "hello" 
+    MDLabel:
+        text: "The Item you entered already exists, would you like to add onto it? "
+        theme_text_color: "Secondary"
+        pos_hint: {"center_y": 0.6}
+
+<Warning>:
+    MDLabel:
+        text: "Are you sure you want to delete this item? "
+        theme_text_color: "Secondary"
+        pos_hint: {"center_y": 0.6}
+<Warning1>:
+    MDLabel:
+        text: "Are you sure you want to delete this item? "
+        theme_text_color: "Secondary"
+        pos_hint: {"center_y": 0.6}
+<Add>:
+    title: "hello" 
+    cost: cost
+    BoxLayout:
+        spacing: "20dp"
+        MDTextField:
+            hint_text: "Add amount:"
+            id:cost
+            text: "$"
+
+"""
+dialogBox2 = """
+Content2:
+    scroll: scroll
+"""
+viewScreen = """
+<viewAll>:
+    ScrollView:
+"""
+
+pannel = """
+"""
---
+++ MyApp.py
@@ -0,0 +1,138 @@
+from kivy import Config
+from KvString import *
+from kivy.graphics.context_instructions import Color
+from kivy.graphics.vertex_instructions import Rectangle
+from kivymd.app import MDApp
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivymd.uix.picker import MDDatePicker,MDThemePicker
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from datetime import date
+from kivy.uix.progressbar import ProgressBar
+from kivy.clock import Clock
+from kivy.core.text import Label as CoreLabel
+import math
+import kivy
+from kivy.graphics import Ellipse, Color, Rectangle
+from kivy.vector import Vector
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+#from math import atan2, sqrt, pow, degrees, sin, cos, radians
+import random
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton
+from kivymd.uix.label import MDLabel
+from kivy.uix.scrollview import ScrollView
+from kivymd.uix.list import MDList, TwoLineListItem
+from kivymd.uix.list import OneLineListItem, TwoLineIconListItem, IconLeftWidget,TwoLineAvatarIconListItem,IconRightWidget
+from kivymd.uix.textfield import MDTextField, MDTextFieldRect
+from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition, NoTransition, FallOutTransition
+from kivymd.uix.expansionpanel import MDExpansionPanel, MDExpansionPanelOneLine, MDExpansionPanelThreeLine
+from kivy.app import App
+
+SizeList=[]
+
+
+#Config.set('graphics','resizable',0)
+Window.size=(400,600)
+
+class MenuScreen(Screen):
+    pass
+
+
+class ProfileScreen(Screen):
+    pass
+
+
+class UploadScreen(Screen):
+    pass
+
+
+class Content2(MDBoxLayout):
+    pass
+class Content3(MDBoxLayout):
+    pass
+
+class Content4(MDBoxLayout):
+    pass
+class Warning(MDBoxLayout):
+    pass
+class Warning1(MDBoxLayout):
+    pass
+class Content1(MDBoxLayout):
+    name = ObjectProperty(None)
+    cost = ObjectProperty(None)
+
+class Add(MDBoxLayout):
+    name = ObjectProperty(None)
+    cost = ObjectProperty(None)
+
+
+class Content(MDBoxLayout):
+    name = ObjectProperty(None)
+    cost = ObjectProperty(None)
+
+
+class help(MDBoxLayout):
+    pass
+class helpTrack(MDBoxLayout):
+    pass
+class helpOverview(MDBoxLayout):
+    pass
+class helpSave(MDBoxLayout):
+    pass
+class viewAll(Screen):
+    pass
+
+
+class Existing(MDBoxLayout):
+    pass
+
+class Content5(MDBoxLayout):
+    pass
+class Bar(Screen):
+    pass
+
+
+
+
+# Create the screen manager
+sm = ScreenManager(transition=FadeTransition())
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(ProfileScreen(name='profile'))
+sm.add_widget(UploadScreen(name='upload'))
+sm.add_widget(Bar(name="Bar"))
+
+
+class MyApp(MDApp):
+
+
+    def build(self):
+
+        self.screen = Builder.load_string(screenHelp)
+
+        return self.screen
+    def erase(self):
+        pass
+    def tab_switchView(self):
+        self.screen.ids.panel.current=("overview")
+        self.Blayout=MDBoxLayout()
+    def tab_switchSave(self):
+        self.screen.ids.panel.current=("save")
+    def tab_switchTrack(self):
+        self.screen.ids.panel.current=("track")
+    def tab_switchRecur(self):
+        self.screen.ids.panel.current=("recur")
+    def changeScreen(self):
+        self.screen.ids.panel.switch_tab("Recurring Payments")
+    def ready(self):
+        pass
+
+
+MyApp().run()
