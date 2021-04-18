b0ba2c33719cbe3c38842e2940b88d77a30f969b
bdesollar <bendesollar01@gmail.com>
2021-04-18 10:42:24 -0500

    Updated Webscraper
    Deleted unused code and now returns true if it works.

--- webScraperFile.py
+++ webScraperFile.py
@@ -21,21 +21,10 @@ class webScrape:
 
     def __init__(self):
         self.driver = webdriver.Chrome(PATH, options=options)
-        # self.driver = webdriver.Chrome(PATH)
         self.driver.get("https://catalog.registrar.uiowa.edu/your-program/")
         webScrape.reqs_list = []
 
     def search_major(self, program):
-        # search_bar = self.driver.find_element_by_id("quicksearch")
-        # search_bar.clear()
-        # search_bar.send_keys(program)
-        # search_bar.send_keys(Keys.RETURN)
-        # program_name = self.driver.find_element_by_class_name("filterimage")
-        # span_name = program_name.find_element_by_tag_name("span")
-        # for x in span_name:
-            # print("Hi")
-        # except:
-            # print("This program does not exist, please enter another")
         no_errors = False
         try:
             buttons = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
@@ -44,20 +33,15 @@ class webScrape:
         except:
             return False
         if no_errors:
-            # for btn in buttons:
-                # btn.click()
-                # break
             WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(
                 (By.XPATH, '//a[text()="Requirements"]'))).click()
             requirements_button = self.driver.find_element_by_id("requirementstexttab")
             requirements_button.click()
-            # course_list = self.driver.find_element_by_class_name("sc_courselist")
             course_odd = self.driver.find_elements_by_class_name('odd')
             for course in course_odd:
                 try:
                     course_info = course.text
                     if len(course_info.split()) != 0 and "these" not in course_info:
-                        print(course_info)
                         webScrape.reqs_list.append(course_info)
                 except:
                     pass
@@ -66,23 +50,17 @@ class webScrape:
                 try:
                     course_info1 = course1.text
                     if len(course_info1.split()) != 0 and "these" not in course_info1:
-                        print(course_info1)
                         webScrape.reqs_list.append(course_info1)
                 except:
                     pass
-            # print(self.driver.title)
-            check_answer = False
-            while not check_answer:
-                user_input = input("Enter 0 to quit: ")
-                if user_input == "0":
-                    check_answer = True
-                    self.driver.close()
-                    print("Hello")
+            return True
 
-
+'''
 start_program = webScrape()
-# user_input1 = input("Enter Major: ")
 user_input1 = "Accounting"
 program_runs = start_program.search_major(user_input1)
 if not program_runs:
     print("Could not find major, please re-enter")
+else:
+    print(start_program.reqs_list)
+'''
